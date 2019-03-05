5dc21a8f9cdc511d1d1ea3bb213592837e8987ae
Ronan Lamour <ronan.lamour@itga.fr>
2019-03-05 18:07:30 +0100

    added cool adjectives

---
+++ data/Store.cs
@@ -0,0 +1,11 @@
+namespace ipsum.data
+{
+    public static class Store
+    {
+        public static string[] CoolAdjectives = {"Atomic", "Gold", "Polar", "Hydrogen", "Thermal", "Energy", "Pure", "Reactive", "Hydraulic", "Hard", "Cold", "Hot", "Super", "Hyper", "Ultra", "Doom", "Burn", "Awesome", "Gigantic", "Strange", "Fear", "Huge", "Meticulous", "Methodical", "Strategic", "Realistic", "Intelligent", "Smart", "Sensible", "Rigorous", "Vigorous", "Innovative", "Unprecedented", "Groundbreaking", "WorldClass", "Robust", "Unique", "Elegant", "Imaginative", "Ideal", "Skilled", "Expert", "Productive", "Skillful", "Adept", "Accomplished", "Significant", "Strong", "Fluent", "Competent", "Genuine", "Determined", "Passionate", "Ambitious", "Attentive", "Responsive", "Precise", "Smooth", "Accurate", "Loyal", "Professional", "Credible", "Honest", "Harmonious", "Eloquent", "Cordial", "Pleasant", "Positive", "Diplomatic", "Calm", "Tolerant", "Charming", "Adaptable", "Comprehensive", "Agile", "Capable", "Flexible", "Prompt", "OpenMinded", "Dexterous"};
+        public static string[] CoolClasses = {"Wizard", "Mesmer", "Guardian", "Necromancer", "Ranger", "Warrior", "Elementalist", "Engineer", "Thief", "Revenant", "Monk", "DeathKnight", "Paladin", "Druid", "Magician", "Hunter", "DemonHunter", "Demon", "Priest", "Mage", "Sorcerer", "Bard", "Cleric", "Barbarian", "Fighter", "Rogue", "Warlock", "BloodHunter"};
+        public static string[] CoolWorks = {""};
+        public static string[] FirstNames = {""};
+        public static string[] LastNames = {""};
+    }
+}
